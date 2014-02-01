e76d8d694578cd11717245dbbfa6a780f07b9a57
nnnnic <ncouilla@adobe.com>
2014-01-31 17:11:14 -0800

    hooks for border radius control

---
+++ tool-hud.js
@@ -0,0 +1,12 @@
+/*
+
+var currentTool = $(".toolbar ul li a").html();
+var overlay = false;
+var currentToolTimer;
+
+*/
+
+function getMe() {
+	var target = document.getElementById(this);
+	alert (target);
+}
